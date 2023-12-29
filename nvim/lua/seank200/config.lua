9a2592cb8a33ba2c1e80badade8005b6136a24b6
Youngwoo Kim <yw.sean.kim@gmail.com>
2023-12-29 16:48:28 +0900

    feat/nvim/migrate config from old repo

--- nvim/lua/seank200/config.lua
+++ nvim/lua/seank200/config.lua
@@ -1,11 +1,17 @@
+-- Line numbering
 vim.opt.nu = true
 vim.opt.rnu = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+-- Indents
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.cmd [[ autocmd FileType c setlocal sw=4 ts=4 ]]
+vim.cmd [[ autocmd FileType cpp setlocal sw=4 ts=4 ]]
+vim.cmd [[ autocmd FileType python setlocal sw=4 ts=4 ]]
+
 
 vim.opt.wrap = false
 
@@ -17,4 +23,3 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 4
 
 vim.opt.colorcolumn = "80"
-
