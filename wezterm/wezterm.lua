7dcac774b3a6772a775ddcb5147de7df001a907a
Youngwoo Kim <youngwoo.kim.200@kt.com>
2024-08-23 15:45:03 +0900

    feat: add bat, delta

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,12 +3,12 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
-config.font_size = 16
+config.font_size = 14
 
 config.color_scheme = 'Catppuccin Macchiato'
 
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE"
 
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
