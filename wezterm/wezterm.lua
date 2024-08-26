8297b20f0927777293ffafe6dbc8f6ce08dae892
Youngwoo Kim <youngwoo.kim.200@kt.com>
2024-08-26 16:38:07 +0900

    updated config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,10 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font_with_fallback { "JetBrainsMono Nerd Font Mono", "Meslo" }
+config.font = wezterm.font_with_fallback {
+  { family = "JetBrainsMono Nerd Font Mono", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } },
+  "Meslo"
+}
 config.font_size = 16
 
 config.color_scheme = 'Catppuccin Macchiato'
@@ -13,7 +16,7 @@ config.enable_tab_bar = false
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
 
-config.initial_cols = 120
-config.initial_rows = 36
+config.initial_cols = 100
+config.initial_rows = 30
 
 return config
