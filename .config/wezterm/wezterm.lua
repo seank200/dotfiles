d9aa74942f14c24a09dc9d46c1c6a5b977adad8c
Youngwoo Kim <yw.sean.kim@gmail.com>
2026-01-28 21:29:32 +0900

    feat: wezterm/change opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,7 +14,7 @@ config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = false
 -- config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.85
 -- config.macos_window_background_blur = 10
 
 config.initial_cols = 120
